9eef3905a3c6ae7c6ecac00a10502de9ee2a4def
Luke Wood <Luke.Wood12@education.nsw.gov.au>
2017-10-18 10:01:41 +1100

    Done Number FUUUUUUN!

---
+++ NumberFun/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberFun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a value of X: ");
+            float X = float.Parse(Console.ReadLine());
+            Console.Write("Enter a value for Y: ");
+            float Y = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{X} + {Y} = {(X + Y)}");
+
+            Console.WriteLine($"{X} x {Y} = {(X * Y)}");
+
+            Console.WriteLine($"{X} - {Y} = {(X - Y)}");
+
+            Console.WriteLine($"{X} / {Y} = {(X / Y)}");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
