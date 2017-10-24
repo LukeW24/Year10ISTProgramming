4cf87a3a57d25d0cef4300a3b7b6e3f6863dfc0e
Luke Wood <Luke.Wood12@education.nsw.gov.au>
2017-10-24 15:01:05 +1100

    Made LoopyLoops!

---
+++ LoopyLoops/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopyLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press enter a number...");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int x = 1; x <= number; x++)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("Press something now...");
+            Console.ReadLine();
+
+            int numbera = 1;
+
+            while (numbera <= 10)
+            {
+                Console.WriteLine(number);
+                number++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
